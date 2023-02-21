bda42e1dc482c4c652a5c0053d15fb4a1a00ac91
sswellington <sswellington@pm.me>
2023-02-21 10:16:01 -0300

    chore: infraestrutura para sqlserver

---
+++ Soube.Infrastructure/Soube.Infrastructure.Database/dbo/Tables/WeatherForecast.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[WeatherForecast] (
+    [Id]          INT          NOT NULL,
+    [Date]        DATE         NOT NULL,
+    [Temperature] INT          NOT NULL,
+    [Summary]     VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
